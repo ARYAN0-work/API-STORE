e12fa76e4b427202c3bc6d138b7b044e427edc6c
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-15 13:30:45 +0000

    feat: adding async-await server flow

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
 export  const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGO_URI!)
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+    } catch (error) {
+        process.exit(1)
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import app from "./app";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening on port ${PORT}`);
-})
+const startServer = async ()=>{
+    try {
+        await connectDB();
+        app.listen(PORT,()=>{
+        })
+    } catch (error) {
+        process.exit(1);
+    }
+}
+
+startServer();
