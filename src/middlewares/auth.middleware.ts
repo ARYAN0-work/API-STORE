6adfabeeb9c749f540724864d0274871ae593b95
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-16 11:58:37 +0000

    feat:adding jwt auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import { Request,Response,NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { User } from "../models/user-model";
+
+export const authenticate = async(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  return res.status(401).json({
+    success: false,
+    message: "Authentication required",
+  });
+ }
+
+ const token = authHeader.split(" ")[1];
+
+ const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+ const user = await User.findById(decoded.userId);
+
+ 
+}
