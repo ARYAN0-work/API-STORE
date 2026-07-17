289ad25d5c0d8da94afb9963137c3d5813fd55e6
ARYAN0-WORK <carrer1270@gmail.com>
2026-07-17 13:51:58 +0000

    feat:adding apiKey genration route

---
+++ src/modules/api-key/apiKey.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { authenticate } from "../../middlewares/auth.middleware"
+import { createApiKey } from "../api-key/apiKey.contoller";
+
+const router = Router();
+
+router.post("/", authenticate , createApiKey);
+
+export default router;
